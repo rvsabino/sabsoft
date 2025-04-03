3df6d8db628d7a679e62f9126a81baf5490b3d08
rsabino <programacao@sabsoft.com.br>
2025-04-03 22:06:16 +0000

    Alteração definitiva da pagina

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Hugo e Patrick se estiverem vendo essa pagina por favor deem uma risadinha</h1>
+    return <h1>A todos que estiverem vendo essa pagina por favor deem uma risadinha</h1>
 }
 
 export default Home;
