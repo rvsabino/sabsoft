9d1ddbbaa266a9a831a0f270767b2f0b8fd14fa5
rsabino <programacao@sabsoft.com.br>
2025-04-03 16:45:57 +0000

    Atualização da pagina principal

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Minha primeira pagina</h1>
+    return <h1>Hugo e Patrick se estiverem vendo essa pagina por favor deem uma risadinha</h1>
 }
 
 export default Home;
