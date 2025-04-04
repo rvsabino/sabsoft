c68e5e43a4988b9a0d5e6926e311a8219657485f
rsabino <programacao@sabsoft.com.br>
2025-04-04 22:18:15 +0000

    Adiciona arquivo '.editorconfig'

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
  
 
 function Home() {
-    return <><h1>Meus amigos, essa é uma pagina que eu criei como teste do curso que eu estou fazendo.</h1><p>Quero muito agradecer o apoio incondicional que recebo de todos voces para continuar nos meus estudos com uma grande possibilidade de dar continuidade nos projetos pessoais que irei criar.</p></>
+    return <><h1>Meus amigos</h1><p>Essa é uma pagina que eu criei como teste do curso que eu estou fazendo. Quero muito agradecer o apoio incondicional que recebo de todos voces para continuar nos meus estudos com uma grande possibilidade de dar continuidade nos projetos pessoais que irei criar.</p></>
  
 }
 
