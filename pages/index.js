6f5f5fc625293a4a75ee7ac7d3bdfc0bb570270e
rsabino <programacao@sabsoft.com.br>
2025-04-04 01:45:14 +0000

    Nova alteração com agradecimentos

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
  
 
 function Home() {
-    return <h1>Olá mundo</h1>
+    return <h1>Meus amigos, essa é uma pagina que eu criei como teste do curso que eu estou fazendo.
+Quero muito agradecer o apoio incondicional que recebo de todos voces para continuar nos meus estudos com uma grande possibilidade de dar continuidade nos projetos pessoais que irei criar.</h1>
  
 }
 
