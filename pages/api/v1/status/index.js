15118972f571cdb9d8bb6e7b1c7adf98d14294ff
rsabino <programacao@sabsoft.com.br>
2025-04-24 00:52:48 +0000

    Finaliza o endpoint de /api/v1/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,39 @@
 import database from "infra/database.js";
+import { Connection } from "pg";
+import { version } from "react";
 
 async function status(request, response) {
-  const result = await database.query("Select 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "São acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databeseVersionValue =
+    await databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databeseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,16 @@
 test("GET to /api/v1/status shoud return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.max_connections);
+
+  const parceUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parceUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("17.4");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
