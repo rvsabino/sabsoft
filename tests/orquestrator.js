05aaefb0ff997b850118b0626c9fe0880abea96e
rsabino <programacao@sabsoft.com.br>
2025-06-02 23:39:10 +0000

    fixando o estilo linting

--- tests/orquestrator.js
+++ tests/orquestrator.js
@@ -6,7 +6,7 @@ async function waitForAllServices() {
   async function waitForAllServices() {
     return retry(fetchStatusPage, {
       retries: 100,
-      maxTimeout: 1000,
+      maxTimeout: 6000,
     });
 
     async function fetchStatusPage() {
