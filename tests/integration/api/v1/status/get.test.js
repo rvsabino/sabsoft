2a4b303c203c63375c68703f5c0ad3f850fe755a
rsabino <programacao@sabsoft.com.br>
2025-06-16 10:17:26 +0000

    Merge branch 'eslint-correcao' into main

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -10,6 +10,8 @@ test("GET to /api/v1/status shoud return 200", async () => {
 
   const responseBody = await response.json();
 
+  const variavelNaoUsada;
+
   expect(responseBody.updated_at).toBeDefined();
   expect(responseBody.max_connections);
 
